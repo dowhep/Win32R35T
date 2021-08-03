881e9103bc9b0a78e2b78b5129778b6ab26ada53
dowhep <caogangshen@gmail.com>
2021-08-03 10:57:17 -0700

    Add project files.

---
+++ Win32R35T/Main.cpp
@@ -0,0 +1,31 @@
+#include <windows.h>
+#include <tchar.h>
+
+int WINAPI WinMain(
+	_In_ HINSTANCE	hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR		lpCmdLine,
+	_In_ int		nCmdShow
+) {
+	WNDCLASSEX wcex;
+
+	wcex.cbSize			= sizeof(WNDCLASSEX);
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= LoadIcon(wcex.hInstance, IDI_APPLICATION);
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName	= NULL;
+	wcex.lpszClassName	= szWindowClass;
+	wcex.hIconSm		= LoadIcon(wcex.hInstance, IDI_APPLICATION);
+};
+
+LRESULT CALLBACK WndProc(
+	_In_ HWND	hWnd,
+	_In_ UINT	message,
+	_In_ WPARAM	wParam,
+	_In_ LPARAM lParam
+);
