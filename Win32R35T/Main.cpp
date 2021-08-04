dcf77a0d604cb62cd7e43d23dea2ca844c10b2bb
dowhep <caogangshen@gmail.com>
2021-08-04 15:42:34 -0700

    following Torial

--- Win32R35T/Main.cpp
+++ Win32R35T/Main.cpp
@@ -5,8 +5,8 @@ int WINAPI WinMain(
 	_In_ HINSTANCE	hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPSTR		lpCmdLine,
-	_In_ int		nCmdShow
-) {
+	_In_ int		nCmdShow)
+{
 	WNDCLASSEX wcex;
 
 	wcex.cbSize			= sizeof(WNDCLASSEX);
@@ -21,11 +21,72 @@ int WINAPI WinMain(
 	wcex.lpszMenuName	= NULL;
 	wcex.lpszClassName	= szWindowClass;
 	wcex.hIconSm		= LoadIcon(wcex.hInstance, IDI_APPLICATION);
+
+	if (!RegisterClassEx(&wcex)) {
+		MessageBox(NULL,
+			_T("Call to RegisterClassEx failed!"),
+			_T("Windows Desktop Guided Tour"),
+			NULL);
+
+		return 1;
+	}
+
+	static TCHAR szWindowClass[] = _T("DesktopApp");
+	static TCHAR szTitle[] = _T("Windows Desktop Guided Tour Application");
+
+	// create window handle
+	HWND hWnd = CreateWindowEx(
+		WS_EX_OVERLAPPEDWINDOW,
+		szWindowClass,
+		szTitle,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		500, 100,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+	if (!hWnd) {
+		MessageBox(NULL,
+			_T("Call to CreateWindowEx failed!"),
+			_T("Windows Desktop Guided Tour"),
+			NULL);
+
+		return 1;
+	}
+
+	// make window visible
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	// create a message loop
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	
+	return (int) msg.wParam;
 };
 
 LRESULT CALLBACK WndProc(
 	_In_ HWND	hWnd,
 	_In_ UINT	message,
 	_In_ WPARAM	wParam,
-	_In_ LPARAM lParam
-);
+	_In_ LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+	TCHAR greeting[] = _T("Hello, Windows desktop!");
+
+	switch (message)
+	{
+	// when window need to redraw
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		TextOut(hdc, 
+			5, 5, 
+			greeting, _tcslen(greeting));
+	}
+};
