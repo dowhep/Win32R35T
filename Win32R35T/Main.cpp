422c485b398edadea8f247dc027ced24bd1f8ca8
dowhep <caogangshen@gmail.com>
2021-08-06 23:41:45 -0700

    get the pixel shader ready

--- Win32R35T/Main.cpp
+++ Win32R35T/Main.cpp
@@ -1,5 +1,15 @@
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <tchar.h>
+#include <d3d11.h>       // D3D interface
+#include <dxgi.h>        // DirectX driver interface
+#include <d3dcompiler.h> // shader compiler
+#include<assert.h>		// assert check and crash when necessary
+
+#pragma comment( lib, "user32" )          // link against the win32 library
+#pragma comment( lib, "d3d11.lib" )       // direct3D library
+#pragma comment( lib, "dxgi.lib" )        // directx graphics interface
+#pragma comment( lib, "d3dcompiler.lib" ) // shader compiler
 
 static TCHAR szWindowClass[] = _T("DesktopApp");
 static TCHAR szTitle[] = _T("Windows Desktop Guided Tour Application");
@@ -8,6 +18,12 @@ HINSTANCE hInst;
 
 // callback for messages
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+// Global Variables
+ID3D11Device* device_ptr = NULL;
+ID3D11DeviceContext* device_context_ptr = NULL;
+IDXGISwapChain* swap_chain_ptr = NULL;
+ID3D11RenderTargetView* render_target_view_ptr = NULL;
 
 int WINAPI WinMain(
 	_In_ HINSTANCE	hInstance,
@@ -42,14 +58,14 @@ int WINAPI WinMain(
 	// store to global var
 	hInst = hInstance;
 
-	// create window handle
+	// create window handle (windows size/options)
 	HWND hWnd = CreateWindowEx(
 		WS_EX_OVERLAPPEDWINDOW,
 		szWindowClass,
 		szTitle,
-		WS_OVERLAPPEDWINDOW,
+		WS_OVERLAPPED | WS_SYSMENU | WS_MINIMIZEBOX,
 		CW_USEDEFAULT, CW_USEDEFAULT,
-		500, 100,
+		384, 432,
 		NULL,
 		NULL,
 		hInstance,
@@ -68,11 +84,118 @@ int WINAPI WinMain(
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
 
+	// create basic swapchain setup
+	DXGI_SWAP_CHAIN_DESC swap_chain_descr = { 0 };
+	swap_chain_descr.BufferDesc.RefreshRate.Numerator = 0;
+	swap_chain_descr.BufferDesc.RefreshRate.Denominator = 1;
+	swap_chain_descr.BufferDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+	swap_chain_descr.SampleDesc.Count = 1;
+	swap_chain_descr.SampleDesc.Quality = 0;
+	swap_chain_descr.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swap_chain_descr.BufferCount = 1;
+	swap_chain_descr.OutputWindow = hWnd;
+	swap_chain_descr.Windowed = true;
+
+	D3D_FEATURE_LEVEL feature_level;
+	UINT flagsD = D3D11_CREATE_DEVICE_SINGLETHREADED;
+#if defined( DEBUG ) || defined( _DEBUG )
+	flagsD |= D3D11_CREATE_DEVICE_DEBUG; // debug outputs
+#endif
+
+	HRESULT hr = D3D11CreateDeviceAndSwapChain(
+		NULL,
+		D3D_DRIVER_TYPE_HARDWARE,
+		NULL,
+		flagsD,
+		NULL,
+		0,
+		D3D11_SDK_VERSION,
+		&swap_chain_descr,
+		&swap_chain_ptr,
+		&device_ptr,
+		&feature_level,
+		&device_context_ptr);
+	assert(S_OK == hr && swap_chain_ptr && device_ptr && device_context_ptr);
+	
+	// fetch view pointers
+	ID3D11Texture2D* framebuffer;
+	hr = swap_chain_ptr->GetBuffer(
+		0,
+		__uuidof(ID3D11Texture2D),
+		(void**)&framebuffer);
+	assert(SUCCEEDED(hr));
+
+	hr = device_ptr->CreateRenderTargetView(
+		framebuffer, 0, &render_target_view_ptr);
+	assert(SUCCEEDED(hr));
+	framebuffer->Release();
+
+	// compile shaders
+	UINT flagsS = D3DCOMPILE_ENABLE_STRICTNESS; 
+#if defined( DEBUG ) || defined( _DEBUG )
+	flagsS |= D3DCOMPILE_DEBUG; // add more debug output
+#endif
+	ID3DBlob* vs_blob_ptr = NULL, * ps_blob_ptr = NULL, * error_blob = NULL;
+
+	// COMPILE VERTEX SHADER
+	HRESULT hrC = D3DCompileFromFile(
+		L"shaders.hlsl",
+		nullptr,
+		D3D_COMPILE_STANDARD_FILE_INCLUDE,
+		"vs_main",
+		"vs_5_0",
+		flagsS,
+		0,
+		&vs_blob_ptr,
+		&error_blob);
+	if (FAILED(hrC)) {
+		if (error_blob) {
+			OutputDebugStringA((char*)error_blob->GetBufferPointer());
+			error_blob->Release();
+		}
+		if (vs_blob_ptr) { vs_blob_ptr->Release(); }
+		assert(false);
+	}
+
+	// COMPILE PIXEL SHADER
+	hrC = D3DCompileFromFile(
+		L"shaders.hlsl",
+		nullptr,
+		D3D_COMPILE_STANDARD_FILE_INCLUDE,
+		"ps_main",
+		"ps_5_0",
+		flagsS,
+		0,
+		&ps_blob_ptr,
+		&error_blob);
+	if (FAILED(hrC)) {
+		if (error_blob) {
+			OutputDebugStringA((char*)error_blob->GetBufferPointer());
+			error_blob->Release();
+		}
+		if (ps_blob_ptr) { ps_blob_ptr->Release(); }
+		assert(false);
+	}
+
 	// create a message loop
 	MSG msg;
-	while (GetMessage(&msg, NULL, 0, 0)) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+	msg.message = WM_NULL;
+	PeekMessage(&msg, NULL, 0U, 0U, PM_NOREMOVE);
+	bool bGotMsg;
+
+	while (WM_QUIT != msg.message) {
+
+		bGotMsg = (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE) != 0);
+
+		if (bGotMsg) {
+			// translate and dispatch
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else {
+
+		}
+
 	}
 	
 	return (int) msg.wParam;
@@ -84,20 +207,8 @@ LRESULT CALLBACK WndProc(
 	_In_ WPARAM	wParam,
 	_In_ LPARAM lParam)
 {
-	PAINTSTRUCT ps;
-	HDC hdc;
-	TCHAR greeting[] = _T("Hello, Windows desktop!");
-
 	switch (message)
 	{
-	// when window need to redraw
-	case WM_PAINT:
-		hdc = BeginPaint(hWnd, &ps);
-		TextOut(hdc, 
-			5, 5, 
-			greeting, _tcslen(greeting));
-		EndPaint(hWnd, &ps);
-		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
